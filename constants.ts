70134aebc54a25b47de8388d6db65859e10fadb0
pfdanilobraga <pfdanilobraga@gmail.com>
2025-10-03 10:45:41 -0300

    Delete constants.ts

--- constants.ts
+++
@@ -1,5 +0,0 @@
-import { DriverGeneralStatus, TripStatus, OvertimeStatus } from './types';
-
-export const GENERAL_STATUS_OPTIONS: string[] = Object.values(DriverGeneralStatus);
-export const TRIP_STATUS_OPTIONS: string[] = Object.values(TripStatus);
-export const OVERTIME_OPTIONS: string[] = Object.values(OvertimeStatus);
